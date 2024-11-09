944031525ce9bd21dc2239752aa599ac6521f4dc
Nick-Corey <ngc45@pitt.edu>
2024-11-09 16:22:20 -0500

    Adding output function and requirements file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Set up Inststruction
+
+- pip install -r requirements.txt
+
+# How to run
+
+- python3 project.py
--- project.py
+++ project.py
@@ -1,9 +1,21 @@
 import json
+import numpy as np
+from tabulate import tabulate
 
 # Create Memory and Register arrays
 Memory = [0] * 32
 Int_Registers = [0] * 32
-Float_Registers = [0] * 32
+Float_Registers = [0.0] * 32
+
+# Creating headers for Output tables
+Int_Registers_Names = [''] * 32
+Float_Registers_Names = [''] * 32
+i = 0
+while i < 32:
+    Int_Registers_Names[i] = "R" + str(i)
+    Float_Registers_Names[i] = "F" + str(i)
+    i = i + 1
+
 
 # TODO: rename from main() to something more specific
 def main():
@@ -25,11 +37,24 @@ def main():
             if "F" in name:  
                 value = float(reg["value"])
                 Float_Registers[int(name[1:])] = value
-    # TODO: Create more detailed output function
-    print(Memory)
-    print(Int_Registers)
-    print(Float_Registers)
+
+    output()
     exit()
+
+def output():
+
+    # Converting memory to numpy array to utilize nonzero() function
+    mem = np.array(Memory)
+    non_zero = np.nonzero(mem)
+
+    # Create headers for memory output
+    mem_headers = list(non_zero[0])
+    mem_headers = ["Mem[" + str(i * 4) + "]" for i in mem_headers]
+
+    # Output Memory
+    print(tabulate([mem[non_zero]], mem_headers, tablefmt="simple_grid"))
+    print(tabulate([Int_Registers], Int_Registers_Names, tablefmt="simple_grid"))
+    print(tabulate([Float_Registers], Float_Registers_Names, tablefmt="simple_grid"))
 
 
 if __name__ == "__main__":
